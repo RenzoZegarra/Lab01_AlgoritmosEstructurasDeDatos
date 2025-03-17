64992842073e5119db264d8ca2c4412bc902ee44
RenzoZegarra <renzolzegarra@gmail.com>
2025-03-16 22:52:22 -0500

    Se hizo una pequeña modificacion con los comentarios.

--- Actividad1/Verificador.java
+++ Actividad1/Verificador.java
@@ -1,12 +1,12 @@
 package Lab01_AlgoritmosEstructurasDeDatos.Actividad1;
 public class Verificador {
-    // Caso 1: ¿Los rectángulos se sobreponen?
+    // Cuando los rectangulos se sobreponen
     public boolean seSobreponen(Rectangulo r1, Rectangulo r2) {
         return (r1.getX1() < r2.getX2() && r1.getX2() > r2.getX1()) &&
            (r1.getY1() < r2.getY2() && r1.getY2() > r2.getY1());
     }
 
-    // Caso 2: ¿Los rectángulos están juntos? (tienen un borde en común pero no se sobreponen)
+    //Cuando los rectangulos estan juntos
     public boolean estanJuntos(Rectangulo r1, Rectangulo r2) {
         boolean juntosVerticalmente = (r1.getX2() == r2.getX1() || r1.getX1() == r2.getX2()) &&
             (r1.getY1() < r2.getY2() && r1.getY2() > r2.getY1());
